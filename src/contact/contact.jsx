a826b9a2659172971830846cd476964c0c73d8ab
Hassan Mohammadi <hassanmohammadi078@gmail.com>
2026-01-09 23:52:38 +0100

    Update contact.jsx

--- src/contact/contact.jsx
+++ src/contact/contact.jsx
@@ -15,9 +15,9 @@ function Contact() {
     <Header />
     <main id="contact">
         <div>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2828.014231114501!2d-0.5549208226107295!3d44.86200507341468!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd552891a2c84faf%3A0x8c835032fab58d20!2s12%20Quai%20du%20S%C3%A9n%C3%A9gal%2C%2033300%20Bordeaux!5e0!3m2!1sen!2sfr!4v1743199676794!5m2!1sen!2sfr" width="100%" title='map' height="auto" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5654.001246387395!2d-0.5725781226101342!3d44.8826362720517!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd552853c5c9ff93%3A0x127589da9714c72b!2s16%20Av.%20des%2040%20Journaux%2C%2033300%20Bordeaux!5e0!3m2!1sen!2sfr!4v1767999113313!5m2!1sen!2sfr" width="100%" title='map' height="auto" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
         <h5 className='i-forcontact'>you can contact me by sending letters also :</h5>
-        <address className='Address'>12 quai du senegal , apt 003 , 33300 Bordeaux</address>
+        <address className='Address'>16 avenue des 40 journaux 33300 bordeaux , Etage : 3 , APT : 5328 </address>
         <p className='i-social'>also take a look to my social media profiles :</p>
         <div id='social-links'>
             <Link to={"https://t.me/mikkarra"}><img src={telegram} alt="" className='social-img' /></Link>
